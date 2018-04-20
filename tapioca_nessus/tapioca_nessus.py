00c62e6c52b3843ba34c10edf7769c29a101e36c
crestani <geislor@gmail.com>
2018-04-20 11:00:24 -0300

    Adiciona headers de autenticação em get_request_kwargs

--- tapioca_nessus/tapioca_nessus.py
+++ tapioca_nessus/tapioca_nessus.py
@@ -1,26 +1,30 @@
 # coding: utf-8
 
+from requests.auth import HTTPBasicAuth
 from tapioca import (
     TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
-
-from requests.auth import HTTPBasicAuth
-
 
 from .resource_mapping import RESOURCE_MAPPING
 
 
 class NessusClientAdapter(JSONAdapterMixin, TapiocaAdapter):
-    api_root = 'https://localhost:8834'
+    api_root = None
     resource_mapping = RESOURCE_MAPPING
+
+    def get_api_root(self, api_params):
+        return api_params.get('api_root', 'https://localhost:8834')
 
     def get_request_kwargs(self, api_params, *args, **kwargs):
         params = super(NessusClientAdapter, self).get_request_kwargs(
             api_params, *args, **kwargs)
 
+        access_key = api_params.get('accessKey')
+        secret_key = api_params.get('secretKey')
 
-        params['auth'] = HTTPBasicAuth(
-            api_params.get('user'), api_params.get('password'))
-
+        if access_key and secret_key:
+            params['headers'].update({'X-Apikeys': f'accessKey={access_key}; secretKey={secret_key}'})
+        else:
+            params['auth'] = HTTPBasicAuth(api_params.get('user'), api_params.get('password'))
 
         return params
 
